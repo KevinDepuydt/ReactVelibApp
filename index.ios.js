34121dffabb7d5dff5c4f5b7ced22ea7ed779247
Kevin Depuydt <kevin@mbp-de-macbook.home>
2016-11-29 22:13:18 +0100

    Add index files

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+/**
+ * Velib Station Locate React App
+ * https://github.com/KevinDepuydt/ReactVelibApp
+ * Kévin Depuydt
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import App from './app/VelibAppIndex';
+
+AppRegistry.registerComponent('VelibApp', () => App);
